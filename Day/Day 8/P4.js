4aee40ac9ef6e69183e0bc81bfb1d8352369aa6b
Deekshitha03518 <Deekshithak069@gmail.com>
2026-07-22 12:55:31 +0530

    //const: variable which cannot be reassigned after its declaration

--- Day/Day 8/P4.js
+++ Day/Day 8/P4.js
@@ -21,3 +21,12 @@ if(true){
     let secret ="Password@123";    //let in JS is block-scoped
 }
 console.log(secret)    //ReferenceError: secret is not defined
+
+//const: variable which cannot be reassigned after its declaration
+if(true){
+    const p1 = 3.14159;
+    //pi = 27    //TYPE ERROR: assignment is constant variable
+
+}
+//const in JS is block-scoped
+console.log(pi)    //Reference error:pi is not defined
