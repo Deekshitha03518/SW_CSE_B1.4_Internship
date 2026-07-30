e0a16bbcc042d4d31fbb78ba91c053970d42e38b
Deekshitha03518 <Deekshithak069@gmail.com>
2026-07-30 10:50:52 +0530

    Day14-react app: List rendering

---
+++ Day/Day 14/day14-react-app/src/components/ListOne.jsx
@@ -0,0 +1,17 @@
+function SimpleList(params) {
+    const items = ["Apple", "Banana", "Cherry", "Mango"];
+    return(
+        <>
+        <div className="list-box">
+            <h3>SimpleList</h3>
+            <ul>
+                {/* map() */}
+                {items.map((item)=>(
+                    <li key={item}>{item}</li>
+                ))}
+            </ul>
+        </div>
+        </>
+    )
+}
+export default SimpleList;
