fa37e703f96279e2d9a0241401f99f45c494864b
Deekshitha03518 <Deekshithak069@gmail.com>
2026-07-30 16:03:51 +0530

    Form ex3

--- Day/Day 14/day14-react-app/src/App.jsx
+++ Day/Day 14/day14-react-app/src/App.jsx
@@ -7,6 +7,7 @@ import UserListHook from "./components/HookTwo";
 import ThemeProviderDemo from "./components/HookThree";
 import SimpleNameForm from "./components/FormOne";
 import SignupForm from "./components/FormTwo";
+import ValidatedForm from "./components/FormThree";
 function App() {
  
 
@@ -20,6 +21,7 @@ function App() {
       <ThemeProviderDemo />
        <SimpleNameForm />
         <SignupForm />
+        <ValidatedForm/>
     </>
   )
 }
