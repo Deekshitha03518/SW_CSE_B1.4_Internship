efdb3051506f35a6d2e66cbaaea748e52945a751
Deekshitha03518 <Deekshithak069@gmail.com>
2026-07-30 15:30:49 +0530

    Form ex1

--- Day/Day 14/day14-react-app/src/App.jsx
+++ Day/Day 14/day14-react-app/src/App.jsx
@@ -5,6 +5,7 @@ import ProductListDemo from "./components/ListThree";
 import  CounterHook  from "./components/HookOne";
 import UserListHook from "./components/HookTwo";
 import ThemeProviderDemo from "./components/HookThree";
+import SimpleNameForm from "./components/FormOne";
 function App() {
  
 
@@ -16,6 +17,7 @@ function App() {
       <CounterHook />
       <UserListHook/>
       <ThemeProviderDemo />
+       <SimpleNameForm />
     </>
   )
 }
